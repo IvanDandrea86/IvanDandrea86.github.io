2f577c832f99629f25a234ac752e2214699fcc18
IvanDandrea86 <ivan.d.andrea1@gmail.com>
2021-11-01 18:21:29 +0100

    cards

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,24 @@
 const menuBtn = document.getElementById("menu")
+const menu = document.getElementById("menu_list")
 const closeBtn = document.getElementById("close")
 var listItem = document.querySelectorAll(".nav_bar_item a")
 console.log(Array.from(listItem))
 let closeMenu = () => {
-    let menu = document.getElementById("menu_list")
+
     menu.style.visibility = "hidden"
     menu.classList.toggle("active")
 }
 menuBtn.addEventListener("click", () => {
-    let menu = document.getElementById("menu_list")
-    menu.style.visibility = "inherit"
+
+    menu.style.visibility = "visible"
     menu.classList.toggle("active")
 })
 closeBtn.addEventListener("click", () => {
-    let menu = document.getElementById("menu_list")
+
     menu.style.visibility = "hidden"
     menu.classList.toggle("active")
 })
+
 Array.from(listItem).forEach((element) => {
     element.addEventListener('click', () => {
         setTimeout(closeMenu, 1500)
