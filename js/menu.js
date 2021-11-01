db41853fcb426ad35e4aa3defb663b47311a8a55
IvanDandrea86 <ivan.d.andrea1@gmail.com>
2021-11-01 12:20:19 +0100

    some animation

--- js/menu.js
+++ js/menu.js
@@ -3,8 +3,10 @@ var closeBtn = document.getElementById("close")
 menuBtn.addEventListener("click", () => {
     let menu = document.getElementById("menu_list")
     menu.style.visibility = "inherit"
+    menu.classList.toggle("active")
 })
 closeBtn.addEventListener("click", () => {
     let menu = document.getElementById("menu_list")
     menu.style.visibility = "hidden"
+    menu.classList.toggle("active")
 })
