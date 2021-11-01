873fbb78dc93f3c3ca142f98ef5347435cad7e92
IvanDandrea86 <ivan.d.andrea1@gmail.com>
2021-11-01 11:56:16 +0100

    menu animation

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,10 @@
-var menuBtn= document.getElementById("menu")
-var closeBtn=document.getElementById("close")
-menuBtn.addEventListener("click",()=>{
-    let menu=document.getElementById("menu_list")
-    menu.style.display="block"
+var menuBtn = document.getElementById("menu")
+var closeBtn = document.getElementById("close")
+menuBtn.addEventListener("click", () => {
+    let menu = document.getElementById("menu_list")
+    menu.style.visibility = "inherit"
 })
-closeBtn.addEventListener("click",()=>{
-    let menu=document.getElementById("menu_list")
-    menu.style.display="none"
+closeBtn.addEventListener("click", () => {
+    let menu = document.getElementById("menu_list")
+    menu.style.visibility = "hidden"
 })
